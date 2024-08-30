99b48fd791da425541f7485ca33cac55f16fe198
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2024-08-30 14:39:54 +0700

    declare a static value and return the data back into json format on web

--- Java-Webapps-Simulation/Dev-Project/SpringTools4_Eclipse/settling-scores/src/main/java/com/bennyjrx/rps/ScoreController.java
+++ Java-Webapps-Simulation/Dev-Project/SpringTools4_Eclipse/settling-scores/src/main/java/com/bennyjrx/rps/ScoreController.java
@@ -12,11 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 public class ScoreController {
+	
+//	This is a property named score that keeps track of the score value in-game
+	static Score score = new Score(30, 20, 10);
 
 //	This is a method that will return string inside browser when somebody enter 127.0.0.1/health-check
 //  With a @GetMapping Annotation
 	@GetMapping("/health-check")
 	public String getHealthCheck() {
 		return "Situation Normal, All Fired Up!";
+	}
+	
+	@GetMapping ("/score") // When browser request 127.0.0.1/score will trigger method below
+//	This is a method that returns a score
+	public Score getScore() {
+//		returning the static score value from line 17
+		return score;
 	}
 }
