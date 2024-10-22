f4b45bae6643588bbe283961659209086c357132
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2024-10-23 05:45:11 +0700

    TaskDAO with implementation using CRUD Repository and JDBC Template

---
+++ Java-Webapps-Simulation/Dev-Project/SpringTools4_Eclipse/SpringBootCrud/src/main/java/com/bennyjrx/spring/data/MagicalTaskDAO.java
@@ -0,0 +1,32 @@
+package com.bennyjrx.spring.data;
+
+import java.util.List;
+
+//Uning CRUD Repository
+public class MagicalTaskDAO implements TaskDAO {
+
+	@Override
+	public void create(Task task) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Task> retrieveALL() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Task task) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Task task) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Java-Webapps-Simulation/Dev-Project/SpringTools4_Eclipse/SpringBootCrud/src/main/java/com/bennyjrx/spring/data/TaskDAO.java
+++ Java-Webapps-Simulation/Dev-Project/SpringTools4_Eclipse/SpringBootCrud/src/main/java/com/bennyjrx/spring/data/TaskDAO.java
@@ -1,6 +1,14 @@
 package com.bennyjrx.spring.data;
+import java.util.List;
 
 public interface TaskDAO {
-	
+//	Create task
+	public void create(Task task);
+//	Read task
+	public List<Task> retrieveALL();
+//	Update task
+	public void update(Task task);
+//	Delete task
+	public void delete(Task task);
 
 }
