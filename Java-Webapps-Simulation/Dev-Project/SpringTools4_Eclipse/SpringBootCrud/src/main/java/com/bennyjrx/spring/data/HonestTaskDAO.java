093221e77100191a7a3d08eb68261e56a83c7831
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2024-10-24 05:25:17 +0700

    able to create data or sql insert from java into mysql

--- Java-Webapps-Simulation/Dev-Project/SpringTools4_Eclipse/SpringBootCrud/src/main/java/com/bennyjrx/spring/data/HonestTaskDAO.java
+++ Java-Webapps-Simulation/Dev-Project/SpringTools4_Eclipse/SpringBootCrud/src/main/java/com/bennyjrx/spring/data/HonestTaskDAO.java
@@ -2,12 +2,22 @@ package com.bennyjrx.spring.data;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 //Using JDBC Template
 public class HonestTaskDAO implements TaskDAO {
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 
 	@Override
 	public void create(Task task) {
 		// TODO Auto-generated method stub
+		// Prepared Statements
+		var sql = "INSERT INTO TASK (name, completed) VALUES (?, ?)";
+		jdbcTemplate.update(sql, task.name, task.completed);
 
 	}
 
--- Java-Webapps-Simulation/Dev-Project/SpringTools4_Eclipse/SpringBootCrud/src/main/java/com/bennyjrx/spring/data/SpringBootCrudApplication.java
+++ Java-Webapps-Simulation/Dev-Project/SpringTools4_Eclipse/SpringBootCrud/src/main/java/com/bennyjrx/spring/data/SpringBootCrudApplication.java
@@ -1,13 +1,24 @@
 package com.bennyjrx.spring.data;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootCrudApplication {
+public class SpringBootCrudApplication implements CommandLineRunner {
+	@Autowired
+	HonestTaskDAO taskDAO;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCrudApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Task task = new Task("Make my bed.", false);
+		taskDAO.create(task);
+		System.out.println("Created!");
 	}
 
 }
