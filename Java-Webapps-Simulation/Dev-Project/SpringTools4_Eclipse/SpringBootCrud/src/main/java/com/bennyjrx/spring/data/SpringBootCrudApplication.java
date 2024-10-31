709652bf615763e3f03b640ce9d16ebc09539742
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2024-10-31 14:03:32 +0700

    CRUD - Read and Update Simulation is Success using TaskRepository MagicalTaskDAO.java

--- Java-Webapps-Simulation/Dev-Project/SpringTools4_Eclipse/SpringBootCrud/src/main/java/com/bennyjrx/spring/data/SpringBootCrudApplication.java
+++ Java-Webapps-Simulation/Dev-Project/SpringTools4_Eclipse/SpringBootCrud/src/main/java/com/bennyjrx/spring/data/SpringBootCrudApplication.java
@@ -1,5 +1,7 @@
 package com.bennyjrx.spring.data;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -25,9 +27,25 @@ public class SpringBootCrudApplication implements CommandLineRunner {
 //	}
 	@Override
 	public void run(String... args) throws Exception {
+//		Creating records inside mysql using object on Task.java, 
+//		MagicalTaskDAO.java and TaskDAO.java
 		Task task = new Task("Do meal prep.", false);
 		taskDAO.create(task);
-		System.out.println("Created!");
+//		Update the records inside mysql using object on Task.java,
+//		MagicalTaskDAO.java and TaskDAO.java
+//		Target the primary key (Id) on mysql to change the data
+		Task updatedTask = new Task(4, "Do meal prep.", true);
+		taskDAO.update(updatedTask);
+//		Read the data inside mysql using object on Task.java,
+//		Target the primary key (Id) on mysql to change the data
+		List<Task> taskList = taskDAO.retrieveALL();
+		for (Task t : taskList) {
+//			Show the data in console
+			System.out.println(t);
+		}
+		
+//		Show that the procedure was done
+		System.out.println("Done!!!");
 	}
 
 }
