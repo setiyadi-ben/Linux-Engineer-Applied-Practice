164cb094edcb8e409f8525f19df4e2d2f075c0e8
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2025-04-25 16:39:24 +0700

    initial commit | konversi data widening casting (otomatis)

---
+++ Java-Development/Dev-Project/VisualStudioCode/belajar-java-dasar/src/KonversiNumber.java
@@ -0,0 +1,18 @@
+public class KonversiNumber {
+    public static void main(String[] args) {
+        // Konversi Tipe Data Number
+    
+        // Widening Casting (Otomatis): byte->short->int->float->double
+        byte iniByte= 10;
+        short iniShort= iniByte; //bisa langsung dikonversi hasilnya
+
+        int iniInt2 = 1000;
+        byte iniByte2 = (byte) iniInt2; // Konversi macam ini akan menimbulkan number overflow
+        // Sederhananya jika nilai int iniInt2 = 1000; mK nilai byte akan mulai dari 0 hingga 127
+        // terus berkurang sampai minus -128 dan akan terus berulang sampai nilai 100 terpenuhi
+
+        // Narrowing Casting (Manual): double ->float->long->int->char->short->byte
+
+    }
+
+}
