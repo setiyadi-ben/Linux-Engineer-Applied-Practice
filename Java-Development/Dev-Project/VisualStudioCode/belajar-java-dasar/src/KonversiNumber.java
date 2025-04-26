0d600959dc42ad61e36649aa690ec0d9d56c3b14
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2025-04-26 15:03:23 +0700

    fix annotation about KonversiNumber.java

--- Java-Development/Dev-Project/VisualStudioCode/belajar-java-dasar/src/KonversiNumber.java
+++ Java-Development/Dev-Project/VisualStudioCode/belajar-java-dasar/src/KonversiNumber.java
@@ -6,12 +6,12 @@ public class KonversiNumber {
         byte iniByte= 10;
         short iniShort= iniByte; //bisa langsung dikonversi hasilnya
 
+        
+        // Narrowing Casting (Manual): double ->float->long->int->char->short->byte
         int iniInt2 = 1000;
         byte iniByte2 = (byte) iniInt2; // Konversi macam ini akan menimbulkan number overflow
         // Sederhananya jika nilai int iniInt2 = 1000; mK nilai byte akan mulai dari 0 hingga 127
         // terus berkurang sampai minus -128 dan akan terus berulang sampai nilai 100 terpenuhi
-
-        // Narrowing Casting (Manual): double ->float->long->int->char->short->byte
 
     }
 
