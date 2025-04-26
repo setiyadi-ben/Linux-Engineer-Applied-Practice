53adb72ef9683aa41eeba1cc9c4288fc6cd80f99
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2025-04-26 15:35:51 +0700

    PZN Tutorial: mempelajari cara print tipe data char (karakter)

---
+++ Java-Development/Dev-Project/VisualStudioCode/belajar-java-dasar/src/TipeDataChar.java
@@ -0,0 +1,18 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char e = 'E';
+        char k = 'K';
+        char o = 'O';
+
+        // print eko kebawah
+        System.out.println(e);
+        System.out.println(k);
+        System.out.println(o);
+
+        // print eko mendatar
+        System.out.print(e);
+        System.out.print(k);
+        System.out.print(o);
+    }
+
+}
