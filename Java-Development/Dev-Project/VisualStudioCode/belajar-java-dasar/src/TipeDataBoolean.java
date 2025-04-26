f2a67fa309050817f0852571307cd2bee705ac2f
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2025-04-26 15:54:58 +0700

    PZN Tutorial: mempelajari tipe data boolean

---
+++ Java-Development/Dev-Project/VisualStudioCode/belajar-java-dasar/src/TipeDataBoolean.java
@@ -0,0 +1,14 @@
+public class TipeDataBoolean {
+    public static void main(String[] args) {
+        //  Tipe data boolean memeliki 2 yaitu benar dan salah
+        // Default value untuk boolean adalah false
+
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println(benar);
+        System.out.println(salah);
+
+    }
+
+}
