2d8279d4161299e296bc5963181d4eedbe515b4b
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2025-05-02 21:32:05 +0700

    memahami penerapan variable

---
+++ Java-Development/Dev-Project/VisualStudioCode/belajar-java-dasar/src/Variable.java
@@ -0,0 +1,27 @@
+public class Variable {
+    public static void main(String[] args) {
+        // Variable adlah tempat untuk menyimpan data
+
+        // Java adalah bahasa static type, sehingga sebuah variable hanya bisa digunakan
+        // untuk menyimpan tipe data yang sama, tidak bisa-berubah-ubah tipe data seperti 
+        // di bahasa pemrograman PHP atau Javascript
+
+        //  Untuk membuat variable di Java kita bisa menggunakan nama tipe data lalu diikuti
+        //  dengan mnama variablenya
+
+        //  Nama variable tidak boleh mengandung whitespace (spasi, enter, tab), dan tidak boleh
+        //  seluruhnya number
+
+        String name;
+        name = "Eko Kurniawan Khannedy";
+
+        int age = 30;
+        String address = "Indonesia";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(address);
+
+    }
+
+}
