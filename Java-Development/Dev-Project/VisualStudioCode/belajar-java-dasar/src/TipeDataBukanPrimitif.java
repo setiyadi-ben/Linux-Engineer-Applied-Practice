b37bd95ecd33bbfa7b9102d34871d718891da2d5
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2025-05-04 21:51:04 +0700

    sampe sini dulu

---
+++ Java-Development/Dev-Project/VisualStudioCode/belajar-java-dasar/src/TipeDataBukanPrimitif.java
@@ -0,0 +1,24 @@
+public class TipeDataBukanPrimitif {
+    public static void main(String[] args) {
+        // PEMROGRAMAN BAHASA JAVA ITU BERORIENTASI Object  ATAU OOP
+        // number, char boolean adalah tipe primitif dan memiliki default value
+        // String bukan tipe data primitif, tidak memiliki default value dan bernilai null
+        // Di Java, semua tipe data primitif memiliki representasi tipe data bukan primitifnya yaitu Object
+        
+        // Representasi Tipe Data Primitif dari primitif ke bukan primitif
+        // byte --> Byte
+        // short --> Short
+        // int --> Integer
+        // long --> Long
+        // float --> Float
+        // double --> Double
+        // char --> Character
+        // boolean --> Boolean
+
+        Integer iniInteger = 100;
+        Long iniLong = 10000L;
+        
+
+    }
+
+}
