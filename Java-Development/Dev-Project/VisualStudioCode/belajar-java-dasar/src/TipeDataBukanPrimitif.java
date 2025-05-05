9d3ac4bd753fdf013bd4d260181f6d002a5d5e4d
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2025-05-05 11:13:23 +0700

    pemahaman mengenai tipe data primitif dan tipedata bukan primitif + catatan

--- Java-Development/Dev-Project/VisualStudioCode/belajar-java-dasar/src/TipeDataBukanPrimitif.java
+++ Java-Development/Dev-Project/VisualStudioCode/belajar-java-dasar/src/TipeDataBukanPrimitif.java
@@ -15,8 +15,33 @@ public class TipeDataBukanPrimitif {
         // char --> Character
         // boolean --> Boolean
 
+        // Contoh penerapan seperti dibawah ini dan berbagai macam variasinya
         Integer iniInteger = 100;
         Long iniLong = 10000L;
+
+        //  Contoh penerapan yang salah Byte iniByte;
+        Byte iniByte = null;
+        System.out.println(iniByte);
+
+        iniByte = 100;
+        System.out.println(iniByte);
+
+        // Menkonversi primitif ke bukan primitif
+        int iniInt = 100;
+        Integer iniInteger2 = iniInt;
+        System.out.println(iniInteger2);
+
+        // Masih primitif ke bukan primitif namun beda tipe data
+        Integer iniObject = iniInt;
+
+        short iniShort = iniObject.shortValue();
+        long iniLong2 = iniObject.longValue();
+        float iniFloat = iniObject.floatValue();
+        System.out.println(iniShort);
+        System.out.println(iniLong2);
+        System.out.println(iniFloat);
+
+        Long amount = 100000L;
         
 
     }
