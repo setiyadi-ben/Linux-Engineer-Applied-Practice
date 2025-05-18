754378345eb1bb3fb533b9c90a3d5e8a1d5402b0
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2025-05-18 14:56:11 +0700

    sampai sini dulu

---
+++ Java-Development/Dev-Project/VisualStudioCode/belajar-java-dasar/src/EkspresiStatementBlock.java
@@ -0,0 +1,37 @@
+public class EkspresiStatementBlock {
+    public static void main(String[] args) {
+        /*
+        Ekspression adalah konstruksi dari variablel, operator dan
+        pemanggilan method yang mengevaluasi menjadi sebuah single value
+        Ekspression merupakan core component dari sebuah statement
+         */
+
+        // Contoh expression
+        // intinya variable yang terdapat 1 value yang dideklarasikan
+        int value;
+        value = 10;
+
+        
+        /*
+         - Statement merupakan kalimat lengkap dalam bahasa
+         - Statement berisikan execution komplit, biasannya diakhiri dengan titik koma.
+         Jenis-jenis statement:
+         1. Assigment expression
+         2. ++ dan --
+         3. Method invocation
+         4. Object creation expression
+         */
+        // Contoh statement
+        System.out.println();
+
+        // Assigment statement
+        double aValue = 8933.234;
+        // Increment statement
+        aValue++;
+        // Method invocation statement
+        System.out.println("Hello World!");
+        // Object creation statement
+        // Date date = new Date();
+
+    }
+}
