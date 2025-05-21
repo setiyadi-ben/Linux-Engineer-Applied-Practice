bff7f96060fff086e7d120eee60024c221d55d59
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2025-05-21 09:22:14 +0700

    memahami for each sebagai pengganti for i loop dalam mengakses data array

--- Java-Development/Dev-Project/VisualStudioCode/belajar-java-dasar/src/ForEach.java
+++ Java-Development/Dev-Project/VisualStudioCode/belajar-java-dasar/src/ForEach.java
@@ -1,15 +1,24 @@
 public class ForEach {
     public static void main(String[] args) {
-        
-        String[] names = {
+
+        /*
+         - For each digunakan untuk mengambil data array sebagai
+         alternatif untuk menggantikan for i loop yang bertele-tele
+         (karena harus membuat counter terhadap data array tersebut)
+         */
+
+        // sampel data array
+         String[] names = {
             "Eko", "Kurniawan", "Khannedy",
             "Programmer", "Zaman", "Now"
         };
         
+        // contoh for i loop yang bertele-tele
         for (var i = 0; i< names.length; i++){
             System.out.println(names[i]);
         }
 
+        // contoh for each yang lebih singkat
         for(var name: names){
             System.out.println(name);
         }
