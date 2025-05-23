3f0ce7e34c720499beb618eaee82f55a5ec94907
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2025-05-23 20:01:27 +0700

    Memahami recursive method dengan menyelesaikan kasus perkalian faktorial

--- Java-Development/Dev-Project/VisualStudioCode/belajar-java-dasar/src/RecursiveMethod.java
+++ Java-Development/Dev-Project/VisualStudioCode/belajar-java-dasar/src/RecursiveMethod.java
@@ -1,6 +1,13 @@
 public class RecursiveMethod {
     public static void main(String[] args) {
+        /*
+         - Recursive method merupakan kemampuan method memanggil dirinya sendiri
+         - Beberapa problem dapat diselesaikan menggunakan recursive method seperti
+            halnya kasus factorial (mengkali bilangan itu sendiri)
+         */
+        // Memanggil method factorial yang menggunakan for loop
         System.out.println(factorialLoop(5));
+        // memanggil method factorial yang menggunakan recursive
         System.out.println(factorialRecursive(5));
     }
 
@@ -8,6 +15,8 @@ public class RecursiveMethod {
     static int factorialLoop(int value){
         var result = 1;
 
+        // counter = 1 dan akan terus naik sampai batas yang ditentukan
+        // oleh System.out.println(factorialLoop(5));
         for(var counter = 1; counter<= value; counter++){
             result *= counter;
         }
@@ -17,8 +26,11 @@ public class RecursiveMethod {
     // Menggunakan metode recursif
     static int factorialRecursive(int value){
         if(value == 1){
+            // jika nilai value setara dengan 1, kembali nilai 1
             return 1;
-        }else{
+        }else{ // jika tidak
+            // kembali nilai value dikali nilai rekursif yang menurun
+            // karena setiap tingkat dikurangi 1
             return value * factorialRecursive(value - 1);
         }
     }
