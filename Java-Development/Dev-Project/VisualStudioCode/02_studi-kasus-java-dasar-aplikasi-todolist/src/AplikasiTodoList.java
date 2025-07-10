304b578cb08c1596af92a854d02142d2aab243fa
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2025-07-10 13:37:18 +0700

    menyelesaikan step 42 s/d 44 - menambah & test TodoList | ralat kemarin yang 41 s/d 46 sebenarnya 34 s/d 42 - menampilkan TodoList

--- Java-Development/Dev-Project/VisualStudioCode/02_studi-kasus-java-dasar-aplikasi-todolist/src/AplikasiTodoList.java
+++ Java-Development/Dev-Project/VisualStudioCode/02_studi-kasus-java-dasar-aplikasi-todolist/src/AplikasiTodoList.java
@@ -12,6 +12,7 @@ public class AplikasiTodoList {
 
         for (var i=0; i<model.length; i++){
             //var todo:String=model[i]; //Cannot use 'var' on variable without initializer
+            // Jangan ditulis begitu, ditulis begini var todo=model[i]; atau seperti dibawah ini:
             String todo=model[i];
             var no=i+1;
 
@@ -22,21 +23,61 @@ public class AplikasiTodoList {
         }
 
     }
-
     /*
-     *  TEST SHOW, ADD, DELETE TODOLIST
+     *  TEST SHOW
      */
-    public static void testShowTodoList() {
-        model[0]="Belajar Java Dasar";
-        model[1]="Studi Kasus Java Dasar : Aplikasi TodoList";
-        showTodoList();
+    // public static void testShowTodoList() {
+    //     model[0]="Belajar Java Dasar";
+    //     model[1]="Studi Kasus Java Dasar : Aplikasi TodoList";
+    //     showTodoList();
         
-    }
+    // }
 
     // menambah "todo" kedalam list
-    public static void addTodoList(){
+    public static void addTodoList(String todo){
+        // Cek apakah model penuh?
+        var isFull=true;
+        for (int i=0; i<model.length; i++){
+            if (model[i]==null){
+                // Jika terdapat model ysng masih kosong
+                // 
+                isFull=false;
+                break;
+            }
+        }
+        // Jika penuh, kita resize ukuran array 2x lipat
+        if (isFull){
+            var temp=model;
+            model=new String[model.length*2];
+
+            for (int i=0; i<temp.length; i++){
+                model[i]=temp[i];
+            }
+        }
+        // Tambahkan ke posisi data index array yang masih NULL
+        for (var i=0; i<model.length; i++){
+            if (model[i]==null){
+                model[i]=todo;
+                break;
+            }
+        }
 
     }
+    /*
+     * TEST SHOW
+     * TEST ADD
+     */
+    public static void testShowTodoList(){
+        // from TEST SHOW
+        model[0]="Belajar Java Dasar";
+        model[1]="Studi Kasus Java Dasar : Aplikasi TodoList";
+        // showTodoList();
+        for (int i=0; i<25; i++){
+            addTodoList("Contoh Todo Ke-" +i);
+        }
+        showTodoList();
+    }
+
 
     // menghapus "todo" kedalam list
     public static void removeTodoList(){
