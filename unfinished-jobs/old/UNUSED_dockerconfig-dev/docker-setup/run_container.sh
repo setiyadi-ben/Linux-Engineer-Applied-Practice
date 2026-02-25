aa72bab8372f29524104a6f82dc0b0caf5b4eb5d
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2026-02-25 11:21:38 +0700

    remove unused folder & files

--- unfinished-jobs/old/UNUSED_dockerconfig-dev/docker-setup/run_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker build -t my_custom_image .
-
-docker run -d \
-  --name l-e-a-p_github_container \
-  --net l-e-a-p_github_network \
-  --ip 192.168.129.129 \
-  l-e-a-p_github_docker_image
