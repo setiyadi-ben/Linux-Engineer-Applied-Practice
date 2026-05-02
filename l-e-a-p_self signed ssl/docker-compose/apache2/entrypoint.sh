1ed0f598391a9c669511e47ee0a516fba050d039
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2026-05-02 12:09:01 +0200

    update self signed ssl cert generator script

--- l-e-a-p_self signed ssl/docker-compose/apache2/entrypoint.sh
+++ l-e-a-p_self signed ssl/docker-compose/apache2/entrypoint.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-# file ini "entrypoint.sh" wajib menggunakan LF
-# di vscode cek kanan bawah dan ganti atau menggunakan dos2unix entrypoint.sh
-# jika di windows masih entrypoint.sh error, lakukan docker compose down dan delete image tomcat & apache lalu docker compose up lagi
 set -e
 
-# Generate SSL certificate if missing
-if [ ! -f /etc/ssl/certs/apache-selfsigned.crt ]; then
+# Generate cert di custom cert directory
+if [ ! -f /usr/local/apache2/ssl/server.crt ]; then
+    mkdir -p /usr/local/apache2/ssl
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-        -keyout /etc/apache2/ssl/apache-selfsigned.key \
-        -out /etc/apache2/ssl/apache-selfsigned.crt \
-        -subj "/C=ID/ST=JAWA TENGAH/L=KOTA SEMARANG/O=SECURE-NET SEMARANG/OU=TELECOM ISP/CN=apache-ubuntux64server-master/emailAddress=support@secure-net.id"
+        -keyout /usr/local/apache2/certs/server.key \
+        -out /usr/local/apache2/certs/server.crt \
+        -subj "/C=ID/ST=JAWA TENGAH/L=KOTA SEMARANG/O=SECURE-NET SEMARANG/OU=TELECOM ISP/CN=leap_apache2-httpd/emailAddress=support@secure-net.id"
 fi
 
-# Validate Apache config
-apachectl configtest
-
-# Start Apache in foreground properly
+httpd -t
 exec "$@"
