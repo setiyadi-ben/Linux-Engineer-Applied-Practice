573f3055c02ca8d22fffa9c627cfd6d7b9302b83
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2026-08-12 05:48:53 +0200

    debloat for lxde desktop environments

---
+++ linux server environment setup/debloat_linux-desktop.sh
@@ -0,0 +1,19 @@
+sudo apt purge -y \
+libreoffice\* \
+atril atril-common \
+galculator \
+xterm \
+vim vim-tiny vim-common vim-runtime \
+hitori \
+lightsoff \
+gpicview \
+xsane xsane-common \
+deluge deluge-common deluge-gtk \
+exfalso \
+quodlibet \
+lxmusic \
+mplayer mplayer-gui mplayer-skin-blue \
+gnome-disk-utility \
+udisks2 libudisks2-0 \
+fdisk gdisk && \
+sudo apt autoremove --purge -y
