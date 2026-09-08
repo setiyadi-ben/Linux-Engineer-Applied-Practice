f8c14a298407a8c84bf1afe61f09934345d0742d
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2026-09-08 09:29:47 +0700

    debloat for lxde

---
+++ linux desktop as daily driver/debloat_linux-desktop.sh
@@ -0,0 +1,19 @@
+sudo apt purge -y \
+libreoffice* \
+atril* \
+galculator \
+xterm \
+vim vim-tiny vim-common vim-runtime \
+hitori \
+lightsoff \
+gpicview \
+xsane xsane-common \
+deluge deluge-common deluge-gtk \
+exfalso \
+quodlibet \
+lxmusic \
+gucharmap \
+gkbd-capplet \
+mplayer mplayer-gui mplayer-skin-blue \
+gnome-disk-utility && \
+sudo apt autoremove --purge -y
