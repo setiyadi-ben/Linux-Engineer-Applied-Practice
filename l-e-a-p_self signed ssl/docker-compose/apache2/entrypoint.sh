0483b9ffe8d90f36a8d4ea1ba82c52dc391aa132
Benny Hartanto Setiyadi <bennyjrx.official@gmail.com>
2026-09-13 05:50:48 +0200

    fix ssl directory missmatch that preventing apache2 to create self signed certificate

--- l-e-a-p_self signed ssl/docker-compose/apache2/entrypoint.sh
+++ l-e-a-p_self signed ssl/docker-compose/apache2/entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 if [ ! -f /usr/local/apache2/ssl/server.crt ]; then
     mkdir -p /usr/local/apache2/ssl
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-        -keyout /usr/local/apache2/certs/server.key \
-        -out /usr/local/apache2/certs/server.crt \
+        -keyout /usr/local/apache2/ssl/server.key \
+        -out /usr/local/apache2/ssl/server.crt \
         -subj "/C=ID/ST=JAWA TENGAH/L=KOTA SEMARANG/O=SECURE-NET SEMARANG/OU=TELECOM ISP/CN=leap_apache2-httpd/emailAddress=support@secure-net.id"
 fi
 
